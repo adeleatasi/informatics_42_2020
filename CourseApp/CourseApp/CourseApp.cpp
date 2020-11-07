bed49dbda7d897c0b61fd236acd5fcc8761b1feb
User of A315 <a315@it.isuct.ru>
2020-11-07 12:01:39 +0400

    laboratornaya

--- CourseApp/CourseApp/CourseApp.cpp
+++ CourseApp/CourseApp/CourseApp.cpp
@@ -2,10 +2,49 @@
 //
 
 #include <iostream>
+using std::cout;
+using std::endl;
+
+double calc(double a, double b, double x)
+{
+	double numerator = log(pow(b, 2)) / log(a);
+	double denominator = pow(abs(pow(x, 2) - pow(a, 2)), 1 / 3.0);
+	double y = numerator / denominator;
+	return y;
+}
+
+void taskA(double a, double b, double xn, double xk, double dx)
+{
+	for (double x = xn; x <= xk; x += dx) {
+		double y = calc(a, b, x);
+		std::cout << x << "\t" << y << std::endl;
+	}
+	return;
+}
+
+void taskB(double a, double b, double *x, int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		double y = calc(a, b, x[i]);
+		//printf;
+		char result[80];
+		sprintf_s(result, "x = %3.5f y = %.2f \n", x[i], y);
+		cout << result << endl;
+
+	}
+}
 
 int main()
-{
-    std::cout << "Hello World!\n";
+{  
+    const double a=2.0;
+    const double b=4.1;
+	const double dx = 0.1;
+	const double xn = 0;
+	const double xk = 10;
+
+	taskA(a, b, xn, xk, dx);
+
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
